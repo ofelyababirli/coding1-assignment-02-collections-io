b41fda0fcf8b40e6d71e0cb7d8581407f7355a5e
ofelyababirli <babirli.ofelya@gmail.com>
2026-09-23 19:39:30 +0400

    changes

--- assignment.py
+++ assignment.py
@@ -1,6 +1,6 @@
-drink = "tea"
-sales = {"tea": 3, "coffee": 5}
-sold = sales[drink]
-sold = sold + 1
-print(drink)
-print(sold)
+drink = "tea"  #it means tea is assigned to variable tea
+sales = {"tea": 3, "coffee": 5}   #it shows the value for sold tea and coffee 
+sold = sales[drink]   # since drink is tea it will show sales for tea and it will be 3 when we run it 
+sold = sold + 1     # it will show 4 
+print(drink)    #it will show tea 
+print(sold)     # it will show 4 becuase it is the last cell 
